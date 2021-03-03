3d3d0532d93e9df8a0cf4350f409aaf3536f37b7
bear157 <jxyee981101@gmail.com>
2021-03-03 23:21:55 +0800

    edit src/ app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <p>Hello Jenkins.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +20,6 @@ function App() {
           Learn React with Jenkins
         </a>
       </header>
-
-      <p>Hello Jenkins.</p>
     </div>
   );
 }
