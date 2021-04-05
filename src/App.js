1e0a5ced11380c49cffcd860b84bf4a9a48f1feb
bear157 <jxyee981101@gmail.com>
2021-04-05 14:36:30 +0800

    commit feature branch

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <p>Hello Jenkins. (gh-pages)</p>
+        <p>Hello Jenkins. (feature branch)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
